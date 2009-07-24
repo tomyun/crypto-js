bdcadfcee559c4961a6ed238627c44c5cf7e8832
Jeff.Mott.OR <Jeff.Mott.OR@eba437e6-77a4-11de-b799-7765b5330b87>
2009-07-24 05:38:19 +0000

    Implemented base64.

--- src/Crypto.js
+++ src/Crypto.js
@@ -1,5 +1,7 @@
 var Crypto = function () {
 
+	var base64map = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
 	return {
 		util: {
 
@@ -132,6 +134,69 @@ var Crypto = function () {
 				for (var c = 0; c < hex.length; c += 2)
 					bytes.push(parseInt(hex.substr(c, 2), 16));
 				return bytes;
+			},
+
+			// Convert a byte array to a base-64 string
+			bytes_base64: function (bytes) {
+
+				var base64 = [],
+				    overflow;
+
+				for (var i = 0; i < bytes.length; i++) {
+					switch (i % 3) {
+						case 0:
+							base64.push(base64map.charAt(bytes[i] >>> 2));
+							overflow = (bytes[i] & 0x3) << 4;
+							break;
+						case 1:
+							base64.push(base64map.charAt(overflow | (bytes[i] >>> 4)));
+							overflow = (bytes[i] & 0xF) << 2;
+							break;
+						case 2:
+							base64.push(base64map.charAt(overflow | (bytes[i] >>> 6)));
+							base64.push(base64map.charAt(bytes[i] & 0x3F));
+							overflow = -1;
+					}
+				}
+
+				// Encode overflow bits, if there are any
+				if (overflow != undefined && overflow != -1)
+					base64.push(base64map.charAt(overflow));
+
+				// Add padding
+				while (base64.length % 4 != 0) base64.push("=");
+
+				return base64.join("");
+
+			},
+
+			// Convert a base-64 string to a byte array
+			base64_bytes: function (base64) {
+
+				// Remove non-base-64 characters
+				base64 = base64.replace(/[^A-Z0-9+\/]/ig, "");
+
+				var bytes = [];
+
+				for (var i = 0; i < base64.length; i++) {
+					switch (i % 4) {
+						case 1:
+							bytes.push((base64map.indexOf(base64.charAt(i - 1)) << 2) |
+							           (base64map.indexOf(base64.charAt(i)) >>> 4));
+							break;
+						case 2:
+							bytes.push(((base64map.indexOf(base64.charAt(i - 1)) & 0xF) << 4) |
+							           (base64map.indexOf(base64.charAt(i)) >>> 2));
+							break;
+						case 3:
+							bytes.push(((base64map.indexOf(base64.charAt(i - 1)) & 0x3) << 6) |
+							           (base64map.indexOf(base64.charAt(i))));
+							break;
+					}
+				}
+
+				return bytes;
+
 			}
 
 		}
