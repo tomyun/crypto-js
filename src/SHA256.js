ba5bcd3b4eb0ec2cc4bd93466040409f3b8b2c29
Jeff.Mott.OR <Jeff.Mott.OR@eba437e6-77a4-11de-b799-7765b5330b87>
2009-07-24 23:50:54 +0000

    Implemented and optimized SHA256.

---
+++ src/SHA256.js
@@ -0,0 +1,125 @@
+Crypto.SHA256 = function () {
+
+	// Shortcut
+	var util = Crypto.util;
+
+	// Public API
+	var self = function (message) {
+		return util.bytes_hex(util.words_bytes(self._SHA256(message)));
+	};
+
+	self.string = function (message) {
+		return util.words_string(self._SHA256(message));
+	};
+
+	self.base64 = function (message) {
+		return util.bytes_base64(util.words_bytes(self._SHA256(message)));
+	};
+
+	// Logical functions
+	self._Ch     = function (x, y, z) { return x & y ^ ~x & z; };
+	self._Maj    = function (x, y, z) { return x & y ^ x & z ^ y & z; };
+	self._Sigma0 = function (x) { return ((x << 30) | (x >>>  2)) ^
+	                                     ((x << 19) | (x >>> 13)) ^
+	                                     ((x << 10) | (x >>> 22)); };
+	self._Sigma1 = function (x) { return ((x << 26) | (x >>>  6)) ^
+	                                     ((x << 21) | (x >>> 11)) ^
+	                                     ((x <<  7) | (x >>> 25)); };
+	self._Gamma0 = function (x) { return ((x << 25) | (x >>>  7)) ^
+	                                     ((x << 14) | (x >>> 18)) ^
+	                                     (x >>>  3); };
+	self._Gamma1 = function (x) { return ((x << 15) | (x >>> 17)) ^
+	                                     ((x << 13) | (x >>> 19)) ^
+	                                     (x >>> 10); };
+
+
+	/**
+	 * The core
+	 */
+
+	// Constants
+	var K = [0x428A2F98, 0x71374491, 0xB5C0FBCF, 0xE9B5DBA5,
+	         0x3956C25B, 0x59F111F1, 0x923F82A4, 0xAB1C5ED5,
+	         0xD807AA98, 0x12835B01, 0x243185BE, 0x550C7DC3,
+	         0x72BE5D74, 0x80DEB1FE, 0x9BDC06A7, 0xC19BF174,
+	         0xE49B69C1, 0xEFBE4786, 0x0FC19DC6, 0x240CA1CC,
+	         0x2DE92C6F, 0x4A7484AA, 0x5CB0A9DC, 0x76F988DA,
+	         0x983E5152, 0xA831C66D, 0xB00327C8, 0xBF597FC7,
+	         0xC6E00BF3, 0xD5A79147, 0x06CA6351, 0x14292967,
+	         0x27B70A85, 0x2E1B2138, 0x4D2C6DFC, 0x53380D13,
+	         0x650A7354, 0x766A0ABB, 0x81C2C92E, 0x92722C85,
+	         0xA2BFE8A1, 0xA81A664B, 0xC24B8B70, 0xC76C51A3,
+	         0xD192E819, 0xD6990624, 0xF40E3585, 0x106AA070,
+	         0x19A4C116, 0x1E376C08, 0x2748774C, 0x34B0BCB5,
+	         0x391C0CB3, 0x4ED8AA4A, 0x5B9CCA4F, 0x682E6FF3,
+	         0x748F82EE, 0x78A5636F, 0x84C87814, 0x8CC70208,
+	         0x90BEFFFA, 0xA4506CEB, 0xBEF9A3F7, 0xC67178F2];
+
+	// Algorithm
+	self._SHA256 = function (message) {
+
+		var m = util.string_words(message),
+		    l = message.length * 8,
+		    HASH = [0x6A09E667, 0xBB67AE85, 0x3C6EF372, 0xA54FF53A,
+		            0x510E527F, 0x9B05688C, 0x1F83D9AB, 0x5BE0CD19],
+		    W = [64],
+		    a, b, c, d, e, f, g, h, i, j,
+		    T1, T2;
+
+		// Padding
+		m[l >> 5] |= 0x80 << (24 - l % 32);
+		m[((l + 64 >> 9) << 4) + 15] = l;
+
+		for (var i = 0; i < m.length; i += 16) {
+
+			a = HASH[0];
+			b = HASH[1];
+			c = HASH[2];
+			d = HASH[3];
+			e = HASH[4];
+			f = HASH[5];
+			g = HASH[6];
+			h = HASH[7];
+
+			for (var j = 0; j < 64; j++) {
+
+				if (j < 16) W[j] = m[j + i];
+				else W[j] = (this._Gamma1(W[j - 2]) >>> 0) +
+				            (W[j - 7] >>> 0) +
+				            (this._Gamma0(W[j - 15]) >>> 0) +
+				            (W[j - 16] >>> 0);
+
+				T1 = (h >>> 0) + (this._Sigma1(e) >>> 0) +
+				     (this._Ch(e, f, g) >>> 0) + (K[j] >>> 0) +
+				     (W[j] >>> 0);
+				T2 = (this._Sigma0(a) >>> 0) + (this._Maj(a, b, c) >>> 0);
+
+				h = g;
+				g = f;
+				f = e;
+				e = d + T1;
+				d = c;
+				c = b;
+				b = a;
+				a = T1 + T2;
+
+			}
+
+			HASH[0] = a + HASH[0];
+			HASH[1] = b + HASH[1];
+			HASH[2] = c + HASH[2];
+			HASH[3] = d + HASH[3];
+			HASH[4] = e + HASH[4];
+			HASH[5] = f + HASH[5];
+			HASH[6] = g + HASH[6];
+			HASH[7] = h + HASH[7];
+
+		}
+
+		return HASH;
+
+	};
+
+	return self;
+
+}();
