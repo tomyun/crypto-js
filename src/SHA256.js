b15e99d96d35b80fa39bb6ff5a238c12e0906721
Jeff.Mott.OR <Jeff.Mott.OR@eba437e6-77a4-11de-b799-7765b5330b87>
2009-07-26 08:21:05 +0000

    Cosmetic.

--- src/SHA256.js
+++ src/SHA256.js
@@ -16,11 +16,6 @@ Crypto.SHA256 = function () {
 		return util.bytes_base64(util.words_bytes(self._SHA256(message)));
 	};
 
-
-	/**
-	 * The core
-	 */
-
 	// Constants
 	var K = [0x428A2F98, 0x71374491, 0xB5C0FBCF, 0xE9B5DBA5,
 	         0x3956C25B, 0x59F111F1, 0x923F82A4, 0xAB1C5ED5,
@@ -39,7 +34,7 @@ Crypto.SHA256 = function () {
 	         0x748F82EE, 0x78A5636F, 0x84C87814, 0x8CC70208,
 	         0x90BEFFFA, 0xA4506CEB, 0xBEF9A3F7, 0xC67178F2];
 
-	// Algorithm
+	// The core
 	self._SHA256 = function (message) {
 
 		var m = util.string_words(message),
