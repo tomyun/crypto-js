0c82610ac8ca3e769930c88b6af0a1476aee7fe1
Jeff.Mott.OR <Jeff.Mott.OR@eba437e6-77a4-11de-b799-7765b5330b87>
2009-07-29 22:34:50 +0000

    Implemented ECB block cipher mode.

---
+++ src/ECB.js
@@ -0,0 +1,44 @@
+Crypto.mode = Crypto.mode || {};
+Crypto.mode.ECB = {
+
+	encrypt: function (cipher, M, K, IV) {
+
+		// Pad
+		M.push(0x80);
+		while (M.length % (cipher._BlockSize * 4) != 0)
+			M.push(0);
+
+		// Encrypt each block
+		for (var i = 0; i < M.length; i += cipher._BlockSize * 4) {
+
+			var block = M.slice(i, i + cipher._BlockSize * 4),
+			    crypted = cipher._EncryptBlock(block, K);
+
+			// Copy crypted bytes into message array
+			for (var j = 0; j < cipher._BlockSize * 4; j++)
+				M[i + j] = crypted[j];
+
+		}
+
+	},
+
+	decrypt: function (cipher, C, K, IV) {
+
+		// Decrypt each block
+		for (var i = 0; i < C.length; i += cipher._BlockSize * 4) {
+
+			var block = C.slice(i, i + cipher._BlockSize * 4),
+			    plain = cipher._DecryptBlock(block, K);
+
+			// Copy plaintext bytes into ciphertext array
+			for (var j = 0; j < cipher._BlockSize * 4; j++)
+				C[i + j] = plain[j];
+
+		}
+
+		// Strip padding
+		do { var popped = C.pop(); } while (popped != 0x80);
+
+	}
+
+};
