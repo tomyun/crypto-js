2c49f3843836d2ce6c9de7af4092c56515ab6b5b
Jeff.Mott.OR <Jeff.Mott.OR@eba437e6-77a4-11de-b799-7765b5330b87>
2009-08-14 22:52:27 +0000

    Fixed algorithm error.

--- src/PBKDF2.js
+++ src/PBKDF2.js
@@ -20,10 +20,11 @@ Crypto.PBKDF2 = function (password, salt, keylen, options) {
 	while (derivedKeyBytes.length < keylen) {
 
 		var block = prf(password, salt + util.bytesToString(
-		                                 util.wordsToBytes([blockindex])));
+		                          util.wordsToBytes([blockindex]))),
+		    u = block;
 		for (var i = 1; i < iterations; i++) {
-			var t = prf(password, util.bytesToString(block));
-			for (var j = 0; j < block.length; j++) block[j] ^= t[j];
+			u = prf(password, util.bytesToString(u));
+			for (var j = 0; j < block.length; j++) block[j] ^= u[j];
 		}
 
 		derivedKeyBytes = derivedKeyBytes.concat(block);
