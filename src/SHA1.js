89979058037e8d251f410728ff6adf2f257ce109
Jeff.Mott.OR <Jeff.Mott.OR@eba437e6-77a4-11de-b799-7765b5330b87>
2010-01-21 18:30:07 +0000

    K-weight optimization.

--- src/SHA1.js
+++ src/SHA1.js
@@ -29,8 +29,8 @@ SHA1._digest = function(message) {
 
 	// Add padding
 	var l = WordArray.getSignificantBytes(m) * 8;
-	m[l >> 5] |= 0x80 << (24 - l % 32);
-	m[((l + 64 >>> 9) << 4) + 15] = l;
+	m[l >> 5] |= 0x80 << 24 - l % 32;
+	m[(l + 64 >>> 9 << 4) + 15] = l;
 
 	// Initial values
 	var w  = [],
@@ -53,20 +53,20 @@ SHA1._digest = function(message) {
 			if (j < 16) w[j] = m[i + j];
 			else {
 				var n = w[j - 3] ^ w[j - 8] ^ w[j - 14] ^ w[j - 16];
-				w[j] = (n << 1) | (n >>> 31);
+				w[j] = n << 1 | n >>> 31;
 			}
 
-			var t = ((H0 << 5) | (H0 >>> 27)) + H4 + (w[j] >>> 0) + (
+			var t = (H0 << 5 | H0 >>> 27) + H4 + (w[j] >>> 0) + (
 			         j < 20 ? (H1 & H2 | ~H1 & H3)           + 0x5A827999 :
 			         j < 40 ? (H1 ^ H2 ^  H3)                + 0x6ED9EBA1 :
 			         j < 60 ? (H1 & H2 |  H1 & H3 | H2 & H3) - 0x70E44324 :
 			                  (H1 ^ H2 ^  H3)                - 0x359D3E2A);
 
-			H4 =  H3;
-			H3 =  H2;
-			H2 = (H1 << 30) | (H1 >>> 2);
-			H1 =  H0;
-			H0 =  t;
+			H4 = H3;
+			H3 = H2;
+			H2 = H1 << 30 | H1 >>> 2;
+			H1 = H0;
+			H0 = t;
 
 		}
 
