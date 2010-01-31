35e6eab61a65a5120be2d6f284b0768fda3fab44
Jeff.Mott.OR <Jeff.Mott.OR@eba437e6-77a4-11de-b799-7765b5330b87>
2010-01-31 04:22:08 +0000

    Updated PBKDF2 source and test suite.

--- src/PBKDF2.js
+++ src/PBKDF2.js
@@ -2,47 +2,51 @@
 
 // Shortcuts
 var C = Crypto,
-    util = C.util,
-    charenc = C.charenc,
-    UTF8 = charenc.UTF8,
-    Binary = charenc.Binary;
+    enc = C.enc,
+    UTF8 = enc.UTF8,
+    Words = enc.Words,
+    WordArray = C.type.WordArray;
 
-C.PBKDF2 = function (password, salt, keylen, options) {
-
-	// Convert to byte arrays
-	if (password.constructor == String) password = UTF8.stringToBytes(password);
-	if (salt.constructor == String) salt = UTF8.stringToBytes(salt);
-	/* else, assume byte arrays already */
+C.PBKDF2 = function(password, salt, keyLength, options) {
 
 	// Defaults
 	var hasher = options && options.hasher || C.SHA1,
-	    iterations = options && options.iterations || 1;
+	    iterations = options && options.iterations || 1,
+	    output = options && options.output || enc.Hex;
 
-	// Pseudo-random function
-	function PRF(password, salt) {
-		return C.HMAC(hasher, salt, password, { asBytes: true });
-	}
+	// Convert to words, else assume words already
+	var p = password.constructor == String ? UTF8.decode(password) : password,
+	    s = salt.constructor == String ? UTF8.decode(salt) : salt;
 
 	// Generate key
-	var derivedKeyBytes = [],
-	    blockindex = 1;
-	while (derivedKeyBytes.length < keylen) {
-		var block = PRF(password, salt.concat(util.wordsToBytes([blockindex])));
+	var derivedKeyWords = [],
+	    blockIndex = 1;
+
+	while (derivedKeyWords.length * 4 < keyLength) {
+
+		var block = PRF(hasher, p, WordArray.concat(s, [blockIndex]));
+
+		// Iterations
 		for (var u = block, i = 1; i < iterations; i++) {
-			u = PRF(password, u);
+			u = PRF(hasher, p, u);
 			for (var j = 0; j < block.length; j++) block[j] ^= u[j];
 		}
-		derivedKeyBytes = derivedKeyBytes.concat(block);
-		blockindex++;
+
+		derivedKeyWords = derivedKeyWords.concat(block);
+		blockIndex++;
+
 	}
 
-	// Truncate excess bytes
-	derivedKeyBytes.length = keylen;
+	// Ignore excess bytes
+	WordArray.setSigBytes(derivedKeyWords, keyLength);
 
-	return options && options.asBytes ? derivedKeyBytes :
-	       options && options.asString ? Binary.bytesToString(derivedKeyBytes) :
-	       util.bytesToHex(derivedKeyBytes);
+	return output.encode(derivedKeyWords);
 
 };
+
+// Pseudo-random function
+function PRF(hasher, password, salt) {
+	return C.HMAC(hasher, salt, password, { output: Words });
+}
 
 })();
