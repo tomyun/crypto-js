ef69386a6f45882d9f1f1ae11aa121a4689aad88
Jeff.Mott.OR <Jeff.Mott.OR@eba437e6-77a4-11de-b799-7765b5330b87>
2011-07-17 05:37:13 +0000

    It isn't necessary for the "Padding" function to copy properties from one object into another. Instead, each padding can just be an object literal.

--- src/Padding.js
+++ src/Padding.js
@@ -2,21 +2,10 @@
  * Crypto-JS contribution from Simon Greatrix
  */
 
-
-// Create pad name space
-Crypto.pad = {};
-
-
-// A Padding object must supply a pad method and a corresponding unpad method.
-Crypto.pad.Padding = function (spec) {
-	this.name = spec.name;
-	this.pad = spec.pad;
-	this.unpad = spec.unpad;
-};
-
-
-// Create padding schemes
 (function () {
+
+    // Create pad namespace
+    var C_pad = Crypto.pad = {};
 
     // Calculate the number of padding bytes required.
     function _requiredPadding(cipher, message) {
@@ -34,18 +23,16 @@ Crypto.pad.Padding = function (spec) {
         };
     
     // No-operation padding, used for stream ciphers
-    Crypto.pad.NoPadding = new Crypto.pad.Padding ({
-    		name : "NoPadding",
+    C_pad.NoPadding = {
     		pad : function (cipher,message) {},
     		unpad : function (message) {}
-    	});
+    	};
 
     // Zero Padding.
     //
     // If the message is not an exact number of blocks, the final block is
     // completed with 0x00 bytes. There is no unpadding.
-    Crypto.pad.ZeroPadding = new Crypto.pad.Padding ({
-    	name : "ZeroPad",
+    C_pad.ZeroPadding = {
 		pad : function (cipher, message) {
             var blockSizeInBytes = cipher._blocksize * 4;
             var reqd = message.length % blockSizeInBytes;
@@ -57,14 +44,13 @@ Crypto.pad.Padding = function (spec) {
         },
         
         unpad : function (message) {}
-    });
+    };
     
     // ISO/IEC 7816-4 padding.
     //
     // Pads the plain text with an 0x80 byte followed by as many 0x00
     // bytes are required to complete the block.
-    Crypto.pad.iso7816 = new Crypto.pad.Padding ({
-    	name : "iso-7816",
+    C_pad.iso7816 = {
         pad : function (cipher, message) {
             var reqd = _requiredPadding(cipher, message);
             message.push(0x80);
@@ -76,14 +62,13 @@ Crypto.pad.Padding = function (spec) {
         unpad : function (message) {
             while (message.pop() != 0x80) {}
         }
-    });
+    };
     
     // ANSI X.923 padding
     //
     // The final block is padded with zeros except for the last byte of the
     // last block which contains the number of padding bytes.
-    Crypto.pad.ansix923 = new Crypto.pad.Padding ({
-    	name : "ansi-x.923",
+    C_pad.ansix923 = {
         pad : function (cipher, message) {
             var reqd = _requiredPadding(cipher, message);
             for (var i = 1; i < reqd; i++) {
@@ -93,14 +78,13 @@ Crypto.pad.Padding = function (spec) {
         },
 
         unpad : _unpadLength
-    });
+    };
 
     // ISO 10126
     //
     // The final block is padded with random bytes except for the last
     // byte of the last block which contains the number of padding bytes.
-    Crypto.pad.iso10126 = new Crypto.pad.Padding ({
-    	name : "iso-10126",
+    C_pad.iso10126 = {
         pad : function (cipher, message) {
             var reqd = _requiredPadding(cipher, message);
             for (var i = 1; i < reqd; i++) {
@@ -110,15 +94,14 @@ Crypto.pad.Padding = function (spec) {
         },
 
         unpad : _unpadLength
-    });
+    };
 
     // PKCS7 padding
     //
     // PKCS7 is described in RFC 5652. Padding is in whole bytes. The
     // value of each added byte is the number of bytes that are added,
     // i.e. N bytes, each of value N are added.
-    Crypto.pad.pkcs7 = new Crypto.pad.Padding ({
-    	name : "pkcs7",
+    C_pad.pkcs7 = {
         pad : function (cipher, message) {
             var reqd = _requiredPadding(cipher, message);
             for (var i = 0; i < reqd; i++) {
@@ -127,10 +110,6 @@ Crypto.pad.Padding = function (spec) {
         },
 
         unpad : _unpadLength
-    });
+    };
 
 })();
-
-
-
-
