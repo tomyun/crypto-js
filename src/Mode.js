457835eac76c6f5850d1368c41b3bfd5c2768009
Jeff.Mott.OR <Jeff.Mott.OR@eba437e6-77a4-11de-b799-7765b5330b87>
2011-07-17 05:32:46 +0000

    Structured modes to use template design pattern: The Mode class defines the skeleton of the algorithm and defers steps to subclasses.

--- src/Mode.js
+++ src/Mode.js
@@ -6,35 +6,35 @@
 
 // Shortcuts
 var C = Crypto,
-    C_mode = C.mode;
+    C_pad = C.pad;
+
+// Create mode namespace
+var C_mode = C.mode = {};
 
 /**
- * Combine a block mode with a padding scheme.
+ * Mode base "class".
  */
-var Mode = C_mode.Mode = function (mode, padding) {
-	this.encryptBlock = mode.encryptBlock;
-	this.decryptBlock = mode.decryptBlock;
-	this.fixOptions = mode.fixOptions;
-	this.name = mode.name;
-	this.padding = padding;
-	if( this.padding == null ) {
-		this.padding = mode.defaultPadding;
-	} else {
-		this.name = mode.name + "/" + this.padding.name;
-	}
+var Mode = C_mode.Mode = function (padding) {
+    if (padding) {
+        this._padding = padding;
+    }
 };
 
 Mode.prototype = {
-	encrypt: function (cipher, m, iv) {
-		this.padding.pad(cipher, m);
-		this.encryptBlock(cipher, m, iv);
-	},
+    encrypt: function (cipher, m, iv) {
+        this._padding.pad(cipher, m);
+        this._doEncrypt(cipher, m, iv);
+    },
 
-	decrypt: function (cipher, m, iv) {
-		this.decryptBlock(cipher, m, iv);
-		this.padding.unpad(m);
-	}
+    decrypt: function (cipher, m, iv) {
+        this._doDecrypt(cipher, m, iv);
+        this._padding.unpad(m);
+    },
+
+    // Default padding
+    _padding: C_pad.iso7816
 };
+
 
 /**
  * Electronic Code Book mode.
@@ -43,31 +43,34 @@ Mode.prototype = {
  * 
  * ECB does not require an initialization vector.
  */
-C_mode.ECB = new Mode({
-	name: "ECB",
-	
-	defaultPadding : Crypto.pad.iso7816,
+var ECB = C_mode.ECB = function () {
+    // Call parent constructor
+    Mode.apply(this, arguments);
+};
 
-    // There is no initialisation vector in ECB
-	fixOptions : function (options) {
-		options.iv = [];
-	},
-		
-    encryptBlock: function (cipher, m, iv) {
-        var blockSizeInBytes = cipher._blocksize * 4;
-        // Encrypt each block
-        for (var offset = 0; offset < m.length; offset += blockSizeInBytes) {
-            cipher._encryptblock(m, offset);
-        }
-    },
-    decryptBlock: function (cipher, c, iv) {
-        var blockSizeInBytes = cipher._blocksize * 4;
-        // Decrypt each block
-        for (var offset = 0; offset < c.length; offset += blockSizeInBytes) {
-            cipher._decryptblock(c, offset);
-        }
+// Inherit from Mode
+var ECB_prototype = ECB.prototype = new Mode;
+
+// Concrete steps for Mode template
+ECB_prototype._doEncrypt = function (cipher, m, iv) {
+    var blockSizeInBytes = cipher._blocksize * 4;
+    // Encrypt each block
+    for (var offset = 0; offset < m.length; offset += blockSizeInBytes) {
+        cipher._encryptblock(m, offset);
     }
-},null);
+};
+ECB_prototype._doDecrypt = function (cipher, c, iv) {
+    var blockSizeInBytes = cipher._blocksize * 4;
+    // Decrypt each block
+    for (var offset = 0; offset < c.length; offset += blockSizeInBytes) {
+        cipher._decryptblock(c, offset);
+    }
+};
+
+// ECB never uses an IV
+ECB_prototype.fixOptions = function (options) {
+    options.iv = [];
+};
 
 
 /**
@@ -76,50 +79,52 @@ C_mode.ECB = new Mode({
  * The first block is XORed with the IV. Subsequent blocks are XOR with the
  * previous cipher output.
  */
-C_mode.CBC = new Mode({
-	name : "CBC",
-	
-    defaultPadding : Crypto.pad.iso7816,
-		
-    encryptBlock: function (cipher, m, iv) {
-        var blockSizeInBytes = cipher._blocksize * 4;
+var CBC = C_mode.CBC = function () {
+    // Call parent constructor
+    Mode.apply(this, arguments);
+};
 
-        // Encrypt each block
-        for (var offset = 0; offset < m.length; offset += blockSizeInBytes) {
-            if (offset == 0) {
-                // XOR first block using IV
-                for (var i = 0; i < blockSizeInBytes; i++)
-                m[i] ^= iv[i];
-            } else {
-                // XOR this block using previous crypted block
-                for (var i = 0; i < blockSizeInBytes; i++)
-                m[offset + i] ^= m[offset + i - blockSizeInBytes];
-            }
-            // Encrypt block
-            cipher._encryptblock(m, offset);
+// Inherit from Mode
+var CBC_prototype = CBC.prototype = new Mode;
+
+// Concrete steps for Mode template
+CBC_prototype._doEncrypt = function (cipher, m, iv) {
+    var blockSizeInBytes = cipher._blocksize * 4;
+
+    // Encrypt each block
+    for (var offset = 0; offset < m.length; offset += blockSizeInBytes) {
+        if (offset == 0) {
+            // XOR first block using IV
+            for (var i = 0; i < blockSizeInBytes; i++)
+            m[i] ^= iv[i];
+        } else {
+            // XOR this block using previous crypted block
+            for (var i = 0; i < blockSizeInBytes; i++)
+            m[offset + i] ^= m[offset + i - blockSizeInBytes];
         }
-    },
-
-    decryptBlock: function (cipher, c, iv) {
-        var blockSizeInBytes = cipher._blocksize * 4;
-
-        // At the start, the previously crypted block is the IV
-        var prevCryptedBlock = iv;
-        
-        // Decrypt each block
-        for (var offset = 0; offset < c.length; offset += blockSizeInBytes) {
-            // Save this crypted block
-            var thisCryptedBlock = c.slice(offset, offset + blockSizeInBytes);
-            // Decrypt block
-            cipher._decryptblock(c, offset);
-            // XOR decrypted block using previous crypted block
-            for (var i = 0; i < blockSizeInBytes; i++) {
-            	c[offset + i] ^= prevCryptedBlock[i];
-            }
-            prevCryptedBlock = thisCryptedBlock;
-        }
+        // Encrypt block
+        cipher._encryptblock(m, offset);
     }
-},null);
+};
+CBC_prototype._doDecrypt = function (cipher, c, iv) {
+    var blockSizeInBytes = cipher._blocksize * 4;
+
+    // At the start, the previously crypted block is the IV
+    var prevCryptedBlock = iv;
+
+    // Decrypt each block
+    for (var offset = 0; offset < c.length; offset += blockSizeInBytes) {
+        // Save this crypted block
+        var thisCryptedBlock = c.slice(offset, offset + blockSizeInBytes);
+        // Decrypt block
+        cipher._decryptblock(c, offset);
+        // XOR decrypted block using previous crypted block
+        for (var i = 0; i < blockSizeInBytes; i++) {
+            c[offset + i] ^= prevCryptedBlock[i];
+        }
+        prevCryptedBlock = thisCryptedBlock;
+    }
+};
 
 
 /**
@@ -131,42 +136,47 @@ C_mode.CBC = new Mode({
  * 
  * This is a stream cipher mode and does not require padding.
  */
-C_mode.CFB = new Mode({
-	name: "CFB",
-	
-	defaultPadding : Crypto.pad.NoPadding,
-		
-    encryptBlock: function (cipher, m, iv) {
-        var blockSizeInBytes = cipher._blocksize * 4,
-            keystream = iv.slice(0);
+var CFB = C_mode.CFB = function () {
+    // Call parent constructor
+    Mode.apply(this, arguments);
+};
 
-        // Encrypt each byte
-        for (var i = 0; i < m.length; i++) {
+// Inherit from Mode
+var CFB_prototype = CFB.prototype = new Mode;
 
-            var j = i % blockSizeInBytes;
-            if( j==0 ) cipher._encryptblock(keystream, 0);
+// Override padding
+CFB_prototype._padding = C_pad.NoPadding;
 
-            m[i] ^= keystream[j];
-            keystream[j] = m[i];
-        }                 
-    },
+// Concrete steps for Mode template
+CFB_prototype._doEncrypt = function (cipher, m, iv) {
+    var blockSizeInBytes = cipher._blocksize * 4,
+        keystream = iv.slice(0);
 
-    decryptBlock: function (cipher, c, iv) {
-        var blockSizeInBytes = cipher._blocksize * 4,
-            keystream = iv.slice(0);
+    // Encrypt each byte
+    for (var i = 0; i < m.length; i++) {
 
-        // Encrypt each byte
-        for (var i = 0; i < c.length; i++) {
+        var j = i % blockSizeInBytes;
+        if (j == 0) cipher._encryptblock(keystream, 0);
 
-            var j = i % blockSizeInBytes;
-            if( j==0 ) cipher._encryptblock(keystream, 0);
-
-            var b = c[i];
-            c[i] ^= keystream[j];
-            keystream[j] = b;
-        }                 
+        m[i] ^= keystream[j];
+        keystream[j] = m[i];
     }
-},null);
+};
+CFB_prototype._doDecrypt = function (cipher, c, iv) {
+    var blockSizeInBytes = cipher._blocksize * 4,
+        keystream = iv.slice(0);
+
+    // Encrypt each byte
+    for (var i = 0; i < c.length; i++) {
+
+        var j = i % blockSizeInBytes;
+        if (j == 0) cipher._encryptblock(keystream, 0);
+
+        var b = c[i];
+        c[i] ^= keystream[j];
+        keystream[j] = b;
+    }
+};
 
 
 /**
@@ -177,29 +187,35 @@ C_mode.CFB = new Mode({
  * 
  * This is a stream cipher mode and does not require padding.
  */
-	Crypto.mode.OFB = new Crypto.mode.Mode({
-		name: "OFB",
-		defaultPadding: Crypto.pad.NoPadding,
-		encryptBlock: OFB,
-		decryptBlock: OFB
-	}, null);
+var OFB = C_mode.OFB = function () {
+    // Call parent constructor
+    Mode.apply(this, arguments);
+};
 
-	// The mode function
-	function OFB(cipher, m, iv) {
+// Inherit from Mode
+var OFB_prototype = OFB.prototype = new Mode;
 
-		var blockSizeInBytes = cipher._blocksize * 4,
-		    keystream = iv.slice(0);
+// Override padding
+OFB_prototype._padding = C_pad.NoPadding;
 
-		// Encrypt each byte
-		for (var i = 0; i < m.length; i++) {
+// Concrete steps for Mode template
+OFB_prototype._doEncrypt = function (cipher, m, iv) {
 
-			// Generate keystream
-			if (i % blockSizeInBytes == 0)
-				cipher._encryptblock(keystream, 0);
+    var blockSizeInBytes = cipher._blocksize * 4,
+        keystream = iv.slice(0);
 
-			// Encrypt byte
-			m[i] ^= keystream[i % blockSizeInBytes];
+    // Encrypt each byte
+    for (var i = 0; i < m.length; i++) {
 
-		}
-	}
+        // Generate keystream
+        if (i % blockSizeInBytes == 0)
+            cipher._encryptblock(keystream, 0);
+
+        // Encrypt byte
+        m[i] ^= keystream[i % blockSizeInBytes];
+
+    }
+};
+OFB_prototype._doDecrypt = OFB_prototype._doEncrypt;
+
 })();
