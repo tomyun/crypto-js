6eb1b7f945949e53745bc619305ca8f9c888272a
Jeff.Mott.OR <Jeff.Mott.OR@eba437e6-77a4-11de-b799-7765b5330b87>
2012-06-21 18:29:53 +0000

    Added comment to delineate auxiliary functions.

--- src/md5.js
+++ src/md5.js
@@ -116,6 +116,7 @@
         }
     });
 
+    // Auxiliary functions
     function FF(a, b, c, d, x, s, t) {
         var n = a + ((b & c) | (~b & d)) + x + t;
         return ((n << s) | (n >>> (32 - s))) + b;
