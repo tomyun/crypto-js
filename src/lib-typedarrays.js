ea19123b959c6bf77f8d56fd1ca041b3161c8175
Jeff.Mott.OR <Jeff.Mott.OR@eba437e6-77a4-11de-b799-7765b5330b87>
2012-10-04 00:21:29 +0000

    Added support for typed arrays. Issue #46

---
+++ src/lib-typedarrays.js
@@ -0,0 +1,47 @@
+(function () {
+    // Shortcuts
+    var C = CryptoJS;
+    var C_lib = C.lib;
+    var WordArray = C_lib.WordArray;
+
+    // Reference original init
+    var $superInit = WordArray.init;
+
+    // Augment WordArray.init to handle typed arrays
+    WordArray.init = function (typedArray) {
+        // Convert buffers to data view
+        if (typedArray instanceof ArrayBuffer) {
+            typedArray = new DataView(typedArray);
+        }
+
+        // Convert array views to data view
+        if (
+            typedArray instanceof Int8Array ||
+            typedArray instanceof Uint8Array ||
+            typedArray instanceof Uint8ClampedArray ||
+            typedArray instanceof Int16Array ||
+            typedArray instanceof Uint16Array ||
+            typedArray instanceof Int32Array ||
+            typedArray instanceof Uint32Array ||
+            typedArray instanceof Float32Array ||
+            typedArray instanceof Float64Array
+        ) {
+            typedArray = new DataView(typedArray.buffer);
+        }
+
+        // Handle data views
+        if (typedArray instanceof DataView) {
+            var typedArrayByteLength = typedArray.byteLength;
+
+            var words = [];
+            for (var i = 0; i < typedArrayByteLength; i++) {
+                words[i >>> 2] |= typedArray.getUint8(i) << (24 - (i % 4) * 8);
+            }
+
+            $superInit.call(this, words, typedArrayByteLength);
+        } else {
+            // Else call normal init
+            $superInit.apply(this, arguments);
+        }
+    };
+}());
