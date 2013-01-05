776c1125d3989276b776faa273ab294490beb8c5
Jeff.Mott.OR <Jeff.Mott.OR@eba437e6-77a4-11de-b799-7765b5330b87>
2013-01-05 22:47:44 +0000

    Optimized k-weight, and other cosmetic changes.

--- src/md5.js
+++ src/md5.js
@@ -8,102 +8,109 @@
     var C_LIB = C.lib;
     var WordArray = C_LIB.WordArray;
     var Hasher = C_LIB.Hasher;
-    var C_ALGO = C.algo;
 
-    // Constants table
-    var T = [];
+    // Rotate constants
+    var R = [
+        7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22,
+        5,  9, 14, 20, 5,  9, 14, 20, 5,  9, 14, 20, 5,  9, 14, 20,
+        4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23,
+        6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21
+    ];
 
-    // Compute constants
+    // Constants table constructed from the sine function
+    var K = [];
     (function () {
         for (var i = 0; i < 64; i++) {
-            T[i] = (Math.abs(Math.sin(i + 1)) * 0x100000000) | 0;
+            K[i] = (Math.abs(Math.sin(i + 1)) * 0x100000000) | 0;
         }
     }());
 
     /**
      * MD5 hash algorithm.
      */
-    var MD5 = C_ALGO.MD5 = Hasher.extend({
-        _doReset: function () {
-            this._hash = WordArray.create([0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476]);
+    var MD5 = C.MD5 = Hasher.extend({
+        _doInit: function () {
         },
 
-        _doProcessBlock: function (M, offset) {
+        _doReset: function () {
+            this._state = [0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476];
+        },
+
+        _doProcessBlock: function (m, offset) {
             // Swap endian
             for (var i = 0; i < 16; i++) {
                 // Shortcuts
                 var wordIndex = offset + i;
-                var word = M[wordIndex];
+                var word = m[wordIndex];
 
                 // Swap
-                M[wordIndex] = (
+                m[wordIndex] = (
                     (((word << 8)  | (word >>> 24)) & 0x00ff00ff) |
                     (((word << 24) | (word >>> 8))  & 0xff00ff00)
                 );
             }
 
             // Shortcut
-            var H = this._hash.words;
+            var state = this._state;
 
             // Working variables
-            var a = H[0];
-            var b = H[1];
-            var c = H[2];
-            var d = H[3];
+            var a = state[0];
+            var b = state[1];
+            var c = state[2];
+            var d = state[3];
 
             // Rounds
-            for (var round = 0; round < 64; round += 4) {
+            for (var round = 0; round < 64; round++) {
                 if (round < 16) {
-                    a = FF(a, b, c, d, M[offset + round],      7, T[round]);
-                    d = FF(d, a, b, c, M[offset + round + 1], 12, T[round + 1]);
-                    c = FF(c, d, a, b, M[offset + round + 2], 17, T[round + 2]);
-                    b = FF(b, c, d, a, M[offset + round + 3], 22, T[round + 3]);
+                    var f = (b & c) | (~b & d);
+                    var g = round;
                 } else if (round < 32) {
-                    a = GG(a, b, c, d, M[offset + ((round + 1) % 16)],   5, T[round]);
-                    d = GG(d, a, b, c, M[offset + ((round + 6) % 16)],   9, T[round + 1]);
-                    c = GG(c, d, a, b, M[offset + ((round + 11) % 16)], 14, T[round + 2]);
-                    b = GG(b, c, d, a, M[offset + (round % 16)],        20, T[round + 3]);
+                    var f = (d & b) | (~d & c);
+                    var g = 5 * round + 1;
                 } else if (round < 48) {
-                    a = HH(a, b, c, d, M[offset + ((round * 3 + 5) % 16)],   4, T[round]);
-                    d = HH(d, a, b, c, M[offset + ((round * 3 + 8) % 16)],  11, T[round + 1]);
-                    c = HH(c, d, a, b, M[offset + ((round * 3 + 11) % 16)], 16, T[round + 2]);
-                    b = HH(b, c, d, a, M[offset + ((round * 3 + 14) % 16)], 23, T[round + 3]);
-                } else /* if (round < 64) */ {
-                    a = II(a, b, c, d, M[offset + ((round * 3) % 16)],       6, T[round]);
-                    d = II(d, a, b, c, M[offset + ((round * 3 + 7) % 16)],  10, T[round + 1]);
-                    c = II(c, d, a, b, M[offset + ((round * 3 + 14) % 16)], 15, T[round + 2]);
-                    b = II(b, c, d, a, M[offset + ((round * 3 + 5) % 16)],  21, T[round + 3]);
+                    var f = b ^ c ^ d;
+                    var g = 3 * round + 5;
+                } else {
+                    var f = c ^ (b | ~d);
+                    var g = 7 * round;
                 }
+
+                var n = a + f + K[round] + m[offset + (g % 16)];
+                var r = R[round];
+
+                var t = d;
+                d = c;
+                c = b;
+                b = (b + ((n << r) | (n >>> (32 - r)))) | 0;
+                a = t;
             }
 
-            // Intermediate hash value
-            H[0] = (H[0] + a) | 0;
-            H[1] = (H[1] + b) | 0;
-            H[2] = (H[2] + c) | 0;
-            H[3] = (H[3] + d) | 0;
+            // Update state
+            state[0] = (state[0] + a) | 0;
+            state[1] = (state[1] + b) | 0;
+            state[2] = (state[2] + c) | 0;
+            state[3] = (state[3] + d) | 0;
         },
 
         _doFinalize: function () {
             // Shortcuts
             var data = this._data;
             var dataWords = data.words;
-
             var nBitsLeft = data.sigBytes * 8;
-
-            var nBitsTotalL = this._nDataBitsL;
-            var nBitsTotalH = this._nDataBitsH;
+            var nBitsTotalLsw = this._nDataBitsLsw;
+            var nBitsTotalMsw = this._nDataBitsMsw;
 
             // Add padding
             dataWords[nBitsLeft >>> 5] |= 0x80 << (24 - nBitsLeft % 32);
 
             var lengthStartIndex = (((nBitsLeft + 64) >>> 9) << 4) + 14;
             dataWords[lengthStartIndex] = (
-                (((nBitsTotalL << 8)  | (nBitsTotalL >>> 24)) & 0x00ff00ff) |
-                (((nBitsTotalL << 24) | (nBitsTotalL >>> 8))  & 0xff00ff00)
+                (((nBitsTotalLsw << 8)  | (nBitsTotalLsw >>> 24)) & 0x00ff00ff) |
+                (((nBitsTotalLsw << 24) | (nBitsTotalLsw >>> 8))  & 0xff00ff00)
             );
             dataWords[lengthStartIndex + 1] = (
-                (((nBitsTotalH << 8)  | (nBitsTotalH >>> 24)) & 0x00ff00ff) |
-                (((nBitsTotalH << 24) | (nBitsTotalH >>> 8))  & 0xff00ff00)
+                (((nBitsTotalMsw << 8)  | (nBitsTotalMsw >>> 24)) & 0x00ff00ff) |
+                (((nBitsTotalMsw << 24) | (nBitsTotalMsw >>> 8))  & 0xff00ff00)
             );
 
             data.sigBytes = dataWords.length * 4;
@@ -112,72 +119,26 @@
             this._process();
 
             // Shortcut
-            var H = this._hash.words;
+            var state = this._state;
 
             // Swap endian
             for (var i = 0; i < 4; i++) {
-                // Shortcut
-                var word = H[i];
-
-                // Swap
-                H[i] = (
+                var word = state[i];
+                state[i] = (
                     (((word << 8)  | (word >>> 24)) & 0x00ff00ff) |
                     (((word << 24) | (word >>> 8))  & 0xff00ff00)
                 );
             }
+
+            // Return final hash
+            return new WordArray(state);
+        },
+
+        clone: function () {
+            var clone = MD5.$super.prototype.clone.call(this);
+            clone._state = clone._state.slice(0);
+
+            return clone;
         }
     });
-
-    // Auxiliary functions
-    function FF(a, b, c, d, x, s, t) {
-        var n = a + ((b & c) | (~b & d)) + x + t;
-        return ((n << s) | (n >>> (32 - s))) + b;
-    }
-
-    function GG(a, b, c, d, x, s, t) {
-        var n = a + ((b & d) | (c & ~d)) + x + t;
-        return ((n << s) | (n >>> (32 - s))) + b;
-    }
-
-    function HH(a, b, c, d, x, s, t) {
-        var n = a + (b ^ c ^ d) + x + t;
-        return ((n << s) | (n >>> (32 - s))) + b;
-    }
-
-    function II(a, b, c, d, x, s, t) {
-        var n = a + (c ^ (b | ~d)) + x + t;
-        return ((n << s) | (n >>> (32 - s))) + b;
-    }
-
-    /**
-     * Shortcut function to the hasher's object interface.
-     *
-     * @param {WordArray|string} message The message to hash.
-     *
-     * @return {WordArray} The hash.
-     *
-     * @static
-     *
-     * @example
-     *
-     *     var hash = CryptoJS.MD5('message');
-     *     var hash = CryptoJS.MD5(wordArray);
-     */
-    C.MD5 = Hasher._createHelper(MD5);
-
-    /**
-     * Shortcut function to the HMAC's object interface.
-     *
-     * @param {WordArray|string} message The message to hash.
-     * @param {WordArray|string} key The secret key.
-     *
-     * @return {WordArray} The HMAC.
-     *
-     * @static
-     *
-     * @example
-     *
-     *     var hmac = CryptoJS.HmacMD5(message, key);
-     */
-    C.HmacMD5 = Hasher._createHmacHelper(MD5);
 }(Math));
