b09c2193a65afb829ffb52766ff85d76fcf2d08f
Jeff.Mott.OR <Jeff.Mott.OR@eba437e6-77a4-11de-b799-7765b5330b87>
2013-01-12 09:02:20 +0000

    Added benchmark tests for latin1 and utf8.

---
+++ test/benchmarks/enc-latin1-benchmark.js
@@ -0,0 +1,81 @@
+YUI.add('enc-latin1-benchmark', function (Y) {
+    Y.CryptoJSTestSuite.add(new Y.Test.Case({
+        name: 'Latin1',
+
+        testStringifyBenchmark: function () {
+            var suite = new Benchmark.Suite();
+
+            suite.add('CryptoJS 4', (function () {
+                var wordArray = CryptoJS.enc.Latin1.parse('\x12\x34\x56\x78\x12\x34\x56\x78');
+
+                return function () {
+                    CryptoJS.enc.Latin1.stringify(wordArray);
+                };
+            }()));
+
+            suite.add('CryptoJS 3', (function () {
+                var wordArray = Y.CryptoJS3.enc.Latin1.parse('\x12\x34\x56\x78\x12\x34\x56\x78');
+
+                return function () {
+                    Y.CryptoJS3.enc.Latin1.stringify(wordArray);
+                }
+            }()));
+
+            suite.add('CryptoJS 2', (function () {
+                var wordArray = Y.CryptoJS2.util.bytesToWords(Y.CryptoJS2.charenc.Binary.stringToBytes('\x12\x34\x56\x78\x12\x34\x56\x78'));
+
+                return function () {
+                    Y.CryptoJS2.charenc.Binary.bytesToString(Y.CryptoJS2.util.wordsToBytes(wordArray));
+                };
+            }()));
+
+            suite.on('cycle', function (e) {
+                Y.log(e.target, 'info', 'TestRunner');
+            });
+
+            suite.on('complete', (function (testCase) {
+                return function () {
+                    testCase.resume(function () {
+                        Y.Assert.areEqual('CryptoJS 4', suite.filter('fastest').pluck('name'));
+                    });
+                }
+            }(this)));
+
+            suite.run({ async: true });
+
+            this.wait(1000000);
+        },
+
+        testParseBenchmark: function () {
+            var suite = new Benchmark.Suite();
+
+            suite.add('CryptoJS 4', function () {
+                CryptoJS.enc.Latin1.parse('\x12\x34\x56\x78\x12\x34\x56\x78');
+            });
+
+            suite.add('CryptoJS 3', function () {
+                Y.CryptoJS3.enc.Latin1.parse('\x12\x34\x56\x78\x12\x34\x56\x78');
+            });
+
+            suite.add('CryptoJS 2', function () {
+                Y.CryptoJS2.util.bytesToWords(Y.CryptoJS2.charenc.Binary.stringToBytes('\x12\x34\x56\x78\x12\x34\x56\x78'));
+            });
+
+            suite.on('cycle', function (e) {
+                Y.log(e.target, 'info', 'TestRunner');
+            });
+
+            suite.on('complete', (function (testCase) {
+                return function () {
+                    testCase.resume(function () {
+                        Y.Assert.areEqual('CryptoJS 4', suite.filter('fastest').pluck('name'));
+                    });
+                }
+            }(this)));
+
+            suite.run({ async: true });
+
+            this.wait(1000000);
+        }
+    }));
+}, '$Rev$');
---
+++ test/benchmarks/enc-utf8-benchmark.js
@@ -0,0 +1,93 @@
+YUI.add('enc-utf8-benchmark', function (Y) {
+    Y.CryptoJSTestSuite.add(new Y.Test.Case({
+        name: 'Utf8',
+
+        testStringifyBenchmark: function () {
+            var suite = new Benchmark.Suite();
+
+            suite.add('CryptoJS 4', (function () {
+                var wordArray = CryptoJS.enc.Utf8.parse('$$$$¢¢€€𤭢');
+
+                return function () {
+                    CryptoJS.enc.Utf8.stringify(wordArray);
+                };
+            }()));
+
+            suite.add('CryptoJS 3', (function () {
+                var wordArray = Y.CryptoJS3.enc.Utf8.parse('$$$$¢¢€€𤭢');
+
+                return function () {
+                    Y.CryptoJS3.enc.Utf8.stringify(wordArray);
+                }
+            }()));
+
+            suite.add('CryptoJS 2', (function () {
+                var wordArray = Y.CryptoJS2.util.bytesToWords(Y.CryptoJS2.charenc.UTF8.stringToBytes('$$$$¢¢€€𤭢'));
+
+                return function () {
+                    Y.CryptoJS2.charenc.UTF8.bytesToString(Y.CryptoJS2.util.wordsToBytes(wordArray));
+                };
+            }()));
+
+            suite.add('sjcl', (function () {
+                var bitArray = Y.Sjcl.codec.utf8String.toBits('$$$$¢¢€€𤭢');
+
+                return function () {
+                    Y.Sjcl.codec.utf8String.fromBits(bitArray);
+                };
+            }()));
+
+            suite.on('cycle', function (e) {
+                Y.log(e.target, 'info', 'TestRunner');
+            });
+
+            suite.on('complete', (function (testCase) {
+                return function () {
+                    testCase.resume(function () {
+                        Y.Assert.areEqual('CryptoJS 4', suite.filter('fastest').pluck('name'));
+                    });
+                }
+            }(this)));
+
+            suite.run({ async: true });
+
+            this.wait(1000000);
+        },
+
+        testParseBenchmark: function () {
+            var suite = new Benchmark.Suite();
+
+            suite.add('CryptoJS 4', function () {
+                CryptoJS.enc.Hex.parse('$$$$¢¢€€𤭢');
+            });
+
+            suite.add('CryptoJS 3', function () {
+                Y.CryptoJS3.enc.Hex.parse('$$$$¢¢€€𤭢');
+            });
+
+            suite.add('CryptoJS 2', function () {
+                Y.CryptoJS2.util.bytesToWords(Y.CryptoJS2.charenc.UTF8.stringToBytes('$$$$¢¢€€𤭢'));
+            });
+
+            suite.add('sjcl', function () {
+                Y.Sjcl.codec.utf8String.toBits('$$$$¢¢€€𤭢');
+            });
+
+            suite.on('cycle', function (e) {
+                Y.log(e.target, 'info', 'TestRunner');
+            });
+
+            suite.on('complete', (function (testCase) {
+                return function () {
+                    testCase.resume(function () {
+                        Y.Assert.areEqual('CryptoJS 4', suite.filter('fastest').pluck('name'));
+                    });
+                }
+            }(this)));
+
+            suite.run({ async: true });
+
+            this.wait(1000000);
+        }
+    }));
+}, '$Rev$');
