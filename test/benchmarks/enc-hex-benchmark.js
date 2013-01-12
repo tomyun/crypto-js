7a8f5aaa37e4e677fe54cef1fa7defa31fbcc4e6
Jeff.Mott.OR <Jeff.Mott.OR@eba437e6-77a4-11de-b799-7765b5330b87>
2013-01-12 03:23:29 +0000

    Added sjcl and wordarray and hex benchmark tests.

---
+++ test/benchmarks/enc-hex-benchmark.js
@@ -0,0 +1,93 @@
+YUI.add('enc-hex-benchmark', function (Y) {
+    Y.CryptoJSTestSuite.add(new Y.Test.Case({
+        name: 'Hex',
+
+        testStringifyBenchmark: function () {
+            var suite = new Benchmark.Suite();
+
+            suite.add('CryptoJS 4', (function () {
+                var wordArray = CryptoJS.enc.Hex.parse('6162636461626364');
+
+                return function () {
+                    CryptoJS.enc.Hex.stringify(wordArray);
+                };
+            }()));
+
+            suite.add('CryptoJS 3', (function () {
+                var wordArray = Y.CryptoJS3.enc.Hex.parse('6162636461626364');
+
+                return function () {
+                    Y.CryptoJS3.enc.Hex.stringify(wordArray);
+                }
+            }()));
+
+            suite.add('CryptoJS 2', (function () {
+                var wordArray = Y.CryptoJS2.util.bytesToWords(Y.CryptoJS2.util.hexToBytes('6162636461626364'));
+
+                return function () {
+                    Y.CryptoJS2.util.bytesToHex(Y.CryptoJS2.util.wordsToBytes(wordArray));
+                };
+            }()));
+
+            suite.add('sjcl', (function () {
+                var bitArray = Y.Sjcl.codec.hex.toBits('6162636461626364');
+
+                return function () {
+                    Y.Sjcl.codec.hex.fromBits(bitArray);
+                };
+            }()));
+
+            suite.on('cycle', function (event) {
+                Y.log(event.target, 'info', 'TestRunner');
+            });
+
+            suite.on('complete', (function (testCase) {
+                return function () {
+                    testCase.resume(function () {
+                        Y.Assert.areEqual('CryptoJS 4', suite.filter('fastest').pluck('name'));
+                    });
+                }
+            }(this)));
+
+            suite.run({ async: true });
+
+            this.wait(1000000);
+        },
+
+        testParseBenchmark: function () {
+            var suite = new Benchmark.Suite();
+
+            suite.add('CryptoJS 4', function () {
+                CryptoJS.enc.Hex.parse('6162636461626364');
+            });
+
+            suite.add('CryptoJS 3', function () {
+                Y.CryptoJS3.enc.Hex.parse('6162636461626364');
+            });
+
+            suite.add('CryptoJS 2', function () {
+                Y.CryptoJS2.util.bytesToWords(Y.CryptoJS2.util.hexToBytes('6162636461626364'));
+            });
+
+            suite.add('sjcl', function () {
+                Y.Sjcl.codec.hex.toBits('6162636461626364');
+            });
+
+            suite.on('cycle', function (event) {
+                Y.log(event.target, 'info', 'TestRunner');
+            });
+
+            suite.on('complete', (function (testCase) {
+                return function () {
+                    testCase.resume(function () {
+                        Y.Assert.areEqual('CryptoJS 4', suite.filter('fastest').pluck('name'));
+                    });
+                }
+            }(this)));
+
+            suite.run({ async: true });
+
+            this.wait(1000000);
+        }
+    }));
+}, '$Rev$');
---
+++ test/benchmarks/lib-wordarray-benchmark.js
@@ -0,0 +1,73 @@
+YUI.add('lib-wordarray-benchmark', function (Y) {
+    Y.CryptoJSTestSuite.add(new Y.Test.Case({
+        name: 'WordArray',
+
+        testConcatBenchmark: function () {
+            var suite = new Benchmark.Suite();
+
+            suite.add('CryptoJS 4', (function () {
+                var wordArray3 = new CryptoJS.lib.WordArray([0x12345678], 3);
+                var wordArray4 = new CryptoJS.lib.WordArray([0x12345678], 4);
+                var wordArray5 = new CryptoJS.lib.WordArray([0x12345678], 5);
+
+                return function () {
+                    (new CryptoJS.lib.WordArray()).concat(wordArray3).concat(wordArray3);
+                    (new CryptoJS.lib.WordArray()).concat(wordArray4).concat(wordArray3);
+                    (new CryptoJS.lib.WordArray()).concat(wordArray5).concat(wordArray3);
+                };
+            }()));
+
+            suite.add('CryptoJS 3', (function () {
+                var wordArray3 = new Y.CryptoJS3.lib.WordArray.init([0x12345678], 3);
+                var wordArray4 = new Y.CryptoJS3.lib.WordArray.init([0x12345678], 4);
+                var wordArray5 = new Y.CryptoJS3.lib.WordArray.init([0x12345678], 5);
+
+                return function () {
+                    (new Y.CryptoJS3.lib.WordArray.init()).concat(wordArray3).concat(wordArray3);
+                    (new Y.CryptoJS3.lib.WordArray.init()).concat(wordArray4).concat(wordArray3);
+                    (new Y.CryptoJS3.lib.WordArray.init()).concat(wordArray5).concat(wordArray3);
+                }
+            }()));
+
+            suite.add('CryptoJS 2', (function () {
+                var byteArray3 = [0x12, 0x34, 0x56];
+                var byteArray4 = [0x12, 0x34, 0x56, 0x78];
+                var byteArray5 = [0x12, 0x34, 0x56, 0x78, 0x00];
+
+                return function () {
+                    [].concat(byteArray3).concat(byteArray3);
+                    [].concat(byteArray4).concat(byteArray3);
+                    [].concat(byteArray5).concat(byteArray3);
+                };
+            }()));
+
+            suite.add('sjcl', (function () {
+                var bitArray3 = [Y.Sjcl.bitArray.partial(24, 0x12345678)];
+                var bitArray4 = [Y.Sjcl.bitArray.partial(32, 0x12345678)];
+                var bitArray5 = [0x12345678, Y.Sjcl.bitArray.partial(8, 0)];
+
+                return function () {
+                    Y.Sjcl.bitArray.concat(Y.Sjcl.bitArray.concat([], bitArray3), bitArray3);
+                    Y.Sjcl.bitArray.concat(Y.Sjcl.bitArray.concat([], bitArray4), bitArray3);
+                    Y.Sjcl.bitArray.concat(Y.Sjcl.bitArray.concat([], bitArray5), bitArray3);
+                };
+            }()));
+
+            suite.on('cycle', function (event) {
+                Y.log(event.target, 'info', 'TestRunner');
+            });
+
+            suite.on('complete', (function (testCase) {
+                return function () {
+                    testCase.resume(function () {
+                        Y.Assert.areEqual('CryptoJS 4', suite.filter('fastest').pluck('name'));
+                    });
+                }
+            }(this)));
+
+            suite.run({ async: true });
+
+            this.wait(1000000);
+        }
+    }));
+}, '$Rev$');
