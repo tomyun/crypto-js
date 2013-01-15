1b111ca8279a481e9ca936580456cfe8cf1c0fc4
Jeff.Mott.OR <Jeff.Mott.OR@eba437e6-77a4-11de-b799-7765b5330b87>
2013-01-15 14:44:11 +0000

    Tagged version 3.1.2.

--- build/components/pad-nopadding-min.js
+++ build/components/pad-nopadding-min.js
@@ -4,4 +4,4 @@ code.google.com/p/crypto-js
 (c) 2009-2013 by Jeff Mott. All rights reserved.
 code.google.com/p/crypto-js/wiki/License
 */
-<?php echo $content ?>
+CryptoJS.pad.NoPadding={pad:function(){},unpad:function(){}};
