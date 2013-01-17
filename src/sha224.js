e1522218b93b8c3dc0be4c113e2ae7d78c62e054
Jeff.Mott.OR <Jeff.Mott.OR@eba437e6-77a4-11de-b799-7765b5330b87>
2013-01-17 00:34:11 +0000

    Updated SHA-224.

--- src/sha224.js
+++ src/sha224.js
@@ -5,58 +5,25 @@
 
     // Shortcuts
     var C = CryptoJS;
-    var C_LIB = C.lib;
-    var WordArray = C_LIB.WordArray;
-    var C_ALGO = C.algo;
-    var SHA256 = C_ALGO.SHA256;
+    var SHA256 = C.SHA256;
 
     /**
      * SHA-224 hash algorithm.
      */
-    var SHA224 = C_ALGO.SHA224 = SHA256.extend({
+    var SHA224 = C.SHA224 = SHA256.extend({
         _doReset: function () {
-            this._hash = WordArray.create([
+            this._state = [
                 0xc1059ed8, 0x367cd507, 0x3070dd17, 0xf70e5939,
                 0xffc00b31, 0x68581511, 0x64f98fa7, 0xbefa4fa4
-            ]);
+            ];
         },
 
         _doFinalize: function () {
-            SHA256._doFinalize.call(this);
+            var hash = SHA224.$super.prototype._doFinalize.call(this);
 
-            this._hash.sigBytes -= 4;
+            hash.sigBytes -= 4;
+
+            return hash;
         }
     });
-
-    /**
-     * Shortcut function to the hasher's object interface.
-     *
-     * @param {WordArray|string} message The message to hash.
-     *
-     * @return {WordArray} The hash.
-     *
-     * @static
-     *
-     * @example
-     *
-     *     var hash = CryptoJS.SHA224('message');
-     *     var hash = CryptoJS.SHA224(wordArray);
-     */
-    C.SHA224 = SHA256._createHelper(SHA224);
-
-    /**
-     * Shortcut function to the HMAC's object interface.
-     *
-     * @param {WordArray|string} message The message to hash.
-     * @param {WordArray|string} key The secret key.
-     *
-     * @return {WordArray} The HMAC.
-     *
-     * @static
-     *
-     * @example
-     *
-     *     var hmac = CryptoJS.HmacSHA224(message, key);
-     */
-    C.HmacSHA224 = SHA256._createHmacHelper(SHA224);
 }());
---
+++ test/benchmarks/sha224-benchmark.js
@@ -0,0 +1,33 @@
+YUI.add('sha224-benchmark', function (Y) {
+    Y.CryptoJSTestSuite.add(new Y.Test.Case({
+        name: 'SHA224',
+
+        testBenchmark: function () {
+            var suite = new Benchmark.Suite();
+
+            suite.add('CryptoJS 4', function () {
+                var hash = CryptoJS.SHA224.hash('abc') + '';
+            });
+
+            suite.add('jsSHA', function () {
+                var hash = (new Y.JsSHA('abc', 'TEXT')).getHash('SHA-224', 'HEX') + '';
+            });
+
+            suite.on('cycle', function (e) {
+                Y.log(e.target, 'info', 'TestRunner');
+            });
+
+            suite.on('complete', (function (testCase) {
+                return function () {
+                    testCase.resume(function () {
+                        Y.Assert.areEqual('CryptoJS 4', suite.filter('fastest').pluck('name'));
+                    });
+                }
+            }(this)));
+
+            suite.run({ async: true });
+
+            this.wait(1000000);
+        }
+    }));
+}, '$Rev$');
